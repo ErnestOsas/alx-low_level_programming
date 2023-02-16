2fad10f3cdd8f1d55d16b142171e5d4a28a660bc
Ernest Akibor <akiborernest@gmail.com>
2023-02-16 07:22:26 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program in C printing a line with the puts function
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+puts("\ "Programming is like building a multilingual puzzle");
+return(0);
+}
+
