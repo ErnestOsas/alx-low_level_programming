3aa564906a8d92c1a49acbbaabc7ff00db794b33
Ernest Akibor <akiborernest@gmail.com>
2023-02-22 07:53:41 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,10 +5,20 @@
  */
 int main(void)
 {
-int num = 1;
-do {
-printf("%d\n", num);
-num++;
+int count;
+unsigned long fib1 = 0, fib2 = 1, num;
+for (count = 0; count < 50; count++)
+{
+num = fib1 + fib2;
+printf("%lu", num);
+fib1 = fib2;
+fib2 = num;
+if (count == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
 } while (num < 50);
 return (0);
 }
