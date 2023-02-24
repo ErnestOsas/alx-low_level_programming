6437e47707dad5c1b1e9ff122e3fac7a9bf0b58e
Ernest Akibor <akiborernest@gmail.com>
2023-02-24 10:19:31 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ int main(void)
 
 		if (j % 3 == 0 && j % 5 != 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (j % 5 == 0 && j % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if (j % 3 == 0 && j % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else if (j == 1)
 		{
@@ -31,7 +31,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d", j);
+			printf(" %d", j);
 		}
 	}
 	printf("\n");
