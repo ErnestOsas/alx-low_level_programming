0eefaa1b545f7edcae621e788e10e87011899e63
Ernest Akibor <69510862+ErnestOsas@users.noreply.github.com>
2023-02-24 17:10:34 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,18 @@
  */
 void more_numbers(void)
 {
-int i, ret;
+	int i, ro;
 
-for (ret = 0; ro < 10; ret++)
-{
-for (i = 0; i <= 14; i++)
-{
-if (i >= 10)
-{
-_putchar((i / 10) + '0');
-}
-_putchar((i % 10) + '0');
-}
-_putchar('\n');
-}
+	for (ro = 0; ro < 10; ro++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
