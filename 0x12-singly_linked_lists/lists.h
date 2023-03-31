9acbb5a2f2200fee0f8c8d54daf304e5ca1d3b8f
Ernest Akibor <akiborernest@gmail.com>
2023-03-31 15:33:58 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,7 +8,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for alx project
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -17,6 +17,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
 size_t print_list(const list_t *h);
-#endif 
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
