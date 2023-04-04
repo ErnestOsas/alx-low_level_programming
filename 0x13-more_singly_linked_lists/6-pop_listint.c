9e7389062ea43d0b8e448fc6bd8a1651ae8546f0
Ernest Akibor <akiborernest@gmail.com>
2023-04-04 12:59:18 -0700

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,14 +9,16 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *temp;
-	int data = 0;
+listint_t *temp;
+int data = 0;
 
-	if (*head != NULL)
-		return (data);
-	temp = *head;
-	data = (*head)->n;
-	*head = (*head)->next;
-	free(temp);
-	return (data);
+if (*head == NULL) 
+{
+return (data);
+}
+temp = *head;
+data = temp->n;
+*head = (*head)->next;
+free(temp);
+return (data);
 }
