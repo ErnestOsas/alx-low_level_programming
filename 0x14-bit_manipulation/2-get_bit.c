1e745c64ec2c3fdc22e389c8e8bd7cc8d255b4fd
Ernest Akibor <akiborernest@gmail.com>
2023-04-10 06:54:39 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,7 +5,7 @@
  * @index: index
  * Return: the bit value
  */
-int get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index) 
 {
-	return ((index > 63) ? -1 : ((n >> index) & 1));
+  return (index > 63) ? -1 : (n >> index) & 1;
 }
