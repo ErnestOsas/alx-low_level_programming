74a71572d86162a814d2bbbe213b1edbedeae8cd
Ernest Akibor <akiborernest@gmail.com>
2023-05-08 08:44:46 -0700

    update1

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,6 @@
 #include <elf.h>
 #include <fcntl.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,33 +13,41 @@
  *
  * Return: 0 if the first n bytes of s1 and s2 are equal, otherwise non-zero
  */
-int _strncmp(const char *s1, const char *s2, size_t n)
+
+int _strncmp(const char *s1, const char *s2, size_t n) 
 {
-	for ( ; n && *s1 && *s2; --n, ++s1, ++s2)
+    while (n > 0 && *s1 && *s2) 
+    {
+        if (*s1 != *s2) 
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-	}
-	if (n)
-	{
-		if (*s1)
-			return (1);
-		if (*s2)
-			return (-1);
-	}
-	return (0);
+            return (*s1 - *s2);
+        }
+        ++s1;
+        ++s2;
+        --n;
+    }
+    if (n == 0) 
+    {
+        return 0;
+    }
+    if (*s1) 
+    {
+        return 1;
+    }
+    return -1;
 }
 
 /**
  * _close - close a file descriptor and print an error message upon failure
  * @fd: the file descriptor to close
  */
-void _close(int fd)
+void _close(int fd) 
 {
-	if (close(fd) != -1)
-		return;
-	write(STDERR_FILENO, "Error: Can't close fd\n", 22);
-	exit(98);
+    if (close(fd) == -1) 
+    {
+        write(STDERR_FILENO, "Error: Can't close fd\n", 22);
+        exit(98);
+    }
 }
 
 /**
@@ -47,33 +56,32 @@ void _close(int fd)
  * @buf: the buffer to write to
  * @count: the number of bytes to read
  */
-void _read(int fd, char *buf, size_t count)
+void _read(int fd, char *buf, size_t count) 
 {
-	if (read(fd, buf, count) != -1)
-		return;
-	write(STDERR_FILENO, "Error: Can't read from file\n", 28);
-	_close(fd);
-	exit(98);
+    if (read(fd, buf, count) == -1) 
+    {
+        write(STDERR_FILENO, "Error: Can't read from file\n", 28);
+        _close(fd);
+        exit(98);
+    }
 }
 
 /**
  * elf_magic - print ELF magic
  * @buffer: the ELF header
  */
-void elf_magic(const unsigned char *buffer)
+void elf_magic(const unsigned char *buffer) 
 {
-	unsigned int i;
+    unsigned int i;
 
-	if (_strncmp((const char *) buffer, ELFMAG, 4))
-	{
-		write(STDERR_FILENO, "Error: Not an ELF file\n", 23);
-		exit(98);
-	}
-
-	printf("ELF Header:\n  Magic:   ");
-
-	for (i = 0; i < 16; ++i)
-		printf("%02x%c", buffer[i], i < 15 ? ' ' : '\n');
+    if (strncmp((const char *) buffer, ELFMAG, 4)) 
+    {
+        write(STDERR_FILENO, "Error: Not an ELF file\n", 23);
+        exit(98);
+    }
+    printf("ELF Header:\n  Magic:   ");
+    for (i = 0; i < 16; ++i)
+        printf("%02x%c", buffer[i], i < 15 ? ' ' : '\n');
 }
 
 /**
@@ -82,22 +90,22 @@ void elf_magic(const unsigned char *buffer)
  *
  * Return: bit mode (32 or 64)
  */
-size_t elf_class(const unsigned char *buffer)
+size_t elf_class(const unsigned char *buffer) 
 {
-	printf("  %-34s ", "Class:");
+    printf("  %-34s ", "Class:");
 
-	if (buffer[EI_CLASS] == ELFCLASS64)
-	{
-		printf("ELF64\n");
-		return (64);
-	}
-	if (buffer[EI_CLASS] == ELFCLASS32)
-	{
-		printf("ELF32\n");
-		return (32);
-	}
-	printf("<unknown: %x>\n", buffer[EI_CLASS]);
-	return (32);
+    if (buffer[EI_CLASS] == ELFCLASS64) 
+    {
+        printf("ELF64\n");
+        return (64);
+    }
+    if (buffer[EI_CLASS] == ELFCLASS32) 
+    {
+        printf("ELF32\n");
+        return (32);
+    }
+    printf("<unknown: %x>\n", buffer[EI_CLASS]);
+    return (32);
 }
 
 /**
@@ -106,36 +114,40 @@ size_t elf_class(const unsigned char *buffer)
  *
  * Return: 1 if big endian, otherwise 0
  */
-int elf_data(const unsigned char *buffer)
+int elf_data(const unsigned char *buffer) 
 {
-	printf("  %-34s ", "Data:");
+    printf("  %-34s ", "Data:");
 
-	if (buffer[EI_DATA] == ELFDATA2MSB)
-	{
-		printf("2's complement, big endian\n");
-		return (1);
-	}
-	if (buffer[EI_DATA] == ELFDATA2LSB)
-	{
-		printf("2's complement, little endian\n");
-		return (0);
-	}
-	printf("Invalid data encoding\n");
-	return (0);
+    if (buffer[EI_DATA] == ELFDATA2MSB) 
+    {
+        printf("2's complement, big endian\n");
+        return (1);
+    }
+    if (buffer[EI_DATA] == ELFDATA2LSB) 
+    {
+        printf("2's complement, little endian\n");
+        return (0);
+    }
+    printf("Invalid data encoding\n");
+    return (0);
 }
 
 /**
  * elf_version - print ELF version
  * @buffer: the ELF header
  */
-void elf_version(const unsigned char *buffer)
+void elf_version(const unsigned char *buffer) 
 {
-	printf("  %-34s %u", "Version:", buffer[EI_VERSION]);
+    printf("  %-34s %u", "Version:", buffer[EI_VERSION]);
 
-	if (buffer[EI_VERSION] == EV_CURRENT)
-		printf(" (current)\n");
-	else
-		printf("\n");
+    if (buffer[EI_VERSION] == EV_CURRENT)
+    {
+        printf(" (current)\n");
+    }
+    else
+    {
+        printf("\n");
+    }
 }
 
 /**
