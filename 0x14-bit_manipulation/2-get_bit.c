47ed27a12d89aa0868147afad169a9173810b9bf
Ernest Akibor <akiborernest@gmail.com>
2023-07-07 04:58:04 -0700

    updated

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * get_bit - Entry Point
+ * @n: input to search
+ * @index: index
+ * Return: the bit value
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+return ((index > 63) ? (int)(-1) : (int) ((n >> index) & 1));
+}
